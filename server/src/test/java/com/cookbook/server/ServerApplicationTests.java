f92559903975b9a6a46e95eb2a3950f9c41875b8
권장순 <jsoonworld@gmail.com>
2025-09-23 22:33:51 +0900

    feat(project) : init server

---
+++ server/src/test/java/com/cookbook/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookbook.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
